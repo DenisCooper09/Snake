708cc754f34a250934d050ab2ada24d3e92f387a
Denis Cooper <denis.cooper09@gmail.com>
2023-04-27 12:49:29 +0100

    Matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
 #include <Arduino.h>
+#include <LedControl.h>
 
-__attribute__((unused)) void setup() {}
+#define PIN_DIN 3
+#define PIN_CS 4
+#define PIN_CLK 5
 
-__attribute__((unused)) void loop() {}
+LedControl matrix = LedControl(PIN_DIN, PIN_CLK, PIN_CS);
+
+void init_matrix() {
+	matrix.shutdown(0, false);
+	matrix.setIntensity(0, 8);
+	matrix.clearDisplay(0);
+}
+
+__attribute__((unused)) void setup() {
+	init_matrix();
+}
+
+__attribute__((unused)) void loop() {
+	static uint32_t led_timer = millis();
+
+	uint8_t led_x, led_y;
+
+	for (led_y = 0; led_y < 8; led_y++) {
+		for (led_x = 0; led_x < 8;) {
+			if (millis() - led_timer >= 100) {
+				led_timer = millis();
+				matrix.setLed(0, led_x, led_y, true);
+				led_x++;
+			}
+		}
+	}
+
+	for (led_y = 0; led_y < 8; led_y++) {
+		for (led_x = 0; led_x < 8;) {
+			if (millis() - led_timer >= 100) {
+				led_timer = millis();
+				matrix.setLed(0, led_x, led_y, false);
+				led_x++;
+			}
+		}
+	}
+}
